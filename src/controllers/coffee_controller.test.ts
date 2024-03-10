4fc90a8c2d8ab90421fb1b7ec273739cac2756f6
Chae Cramb <chaecramb@gmail.com>
2024-03-10 16:12:58 +0000

    test: Add tests for further paths

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -8,3 +8,21 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee should return correct object when passed a different coffeeName", async () => {
+  const res = await request(app).get("/coffee").query({ coffeeName: "Mocha" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Mocha",
+  });
+});
+
+test("GET /coffee should return the default when coffee name is not specified", async () => {
+  const res = await request(app).get("/coffee").query({});
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
